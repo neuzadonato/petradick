659c6b51792f9d43c03f6b74b391c31033ee00f8
neuzadonato <neuza.donato@gmail.com>
2023-03-15 22:28:39 -0300

    criação de modelo, banco de dados e migrations

---
+++ app/Models/Pet.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pet extends Model
+{
+    use HasFactory;
+
+ // Indica o nome da tabela do banco de dados
+ protected $table = "pets";
+
+ // Indica os campos que podem ser preenchidos pelo usuário
+ protected $fillable = [
+     'nome',
+     'raca',
+     'idade',
+     'sexo',
+     'cor',
+     'petgrafia',
+     'foto'
+ ];
+}
---
+++ database/migrations/2023_03_16_005450_create_pets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('raca', 100);
+            $table->integer('idade');
+            $table->string('sexo', 100);
+            $table->string('cor', 100);
+            $table->string('petgrafia', 100);
+            $table->string('foto', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+};
